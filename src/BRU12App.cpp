a56fc230b37a9b32996641d9538452615d5e6b72
Vladimir Mitrović <v@brutalism.rs>
2017-03-06 00:15:47 +0100

    Great success, mesh from openvdb volume

--- src/BRU12App.cpp
+++ src/BRU12App.cpp
@@ -1,4 +1,6 @@
 #include <openvdb/openvdb.h>
+#include <openvdb/tools/VolumeToMesh.h>
+#include <openvdb/tools/LevelSetSphere.h>
 
 #include "cinder/app/App.h"
 #include "cinder/app/RendererGl.h"
@@ -18,6 +20,9 @@ using namespace std;
 #define HEIGHT 768
 #define NODE_COUNT 80
 #define INDEX_COUNT NODE_COUNT * 3
+
+typedef openvdb::tree::Tree4<float, 5, 4, 3>::Type FloatTreeType;
+typedef openvdb::Grid<FloatTreeType> FloatGridType;
 
 class BRU12App : public App {
 public:
@@ -40,10 +45,12 @@ public:
     gl::VboRef indexVbo;
     gl::VboMeshRef mesh;
 //    gl::BatchRef meshBatch;
+    gl::VboMeshRef volumeMesh;
 
 private:
     void recreateVBOs();
     void initializeOpenVDB();
+    void gridToMesh(const FloatGridType& grid, vector<openvdb::Vec3s>& points, vector<openvdb::Vec3I>& triangles);
 };
 
 BRU12App::BRU12App() : process(NODE_COUNT), camera(WIDTH, HEIGHT, 90, 0.0001, 10000) {}
@@ -74,41 +81,66 @@ void BRU12App::setup() {
 }
 
 void BRU12App::initializeOpenVDB() {
-    // Initialize the OpenVDB library.  This must be called at least
-    // once per program and may safely be called multiple times.
     openvdb::initialize();
-    // Create an empty floating-point grid with background value 0.
-    openvdb::FloatGrid::Ptr grid = openvdb::FloatGrid::create();
-    std::cout << "Testing random access:" << std::endl;
-    // Get an accessor for coordinate-based access to voxels.
-    openvdb::FloatGrid::Accessor accessor = grid->getAccessor();
-    // Define a coordinate with large signed indices.
-    openvdb::Coord xyz(1000, -200000000, 30000000);
 
-    // Set the voxel value at (1000, -200000000, 30000000) to 1.
-    accessor.setValue(xyz, 1.0);
+    FloatGridType grid(1.0f);
+    openvdb::CoordBBox bbox(0.0f, 0.0f, 0.0f, 8.0f, 2.0f, 1.0f);
+    grid.tree().fill(bbox, -1.0f);
 
-    // Verify that the voxel value at (1000, -200000000, 30000000) is 1.
-    std::cout << "Grid" << xyz << " = " << accessor.getValue(xyz) << std::endl;
+    vector<openvdb::Vec3s> points;
+    vector<openvdb::Vec3I> triangles;
+    gridToMesh(grid, points, triangles);
 
-    // Reset the coordinates to those of a different voxel.
-    xyz.reset(1000, 200000000, -30000000);
+    geom::BufferLayout layout;
+    layout.append(geom::Attrib::POSITION, 3, sizeof(openvdb::Vec3s), 0);
+    auto vbo = gl::Vbo::create(GL_ARRAY_BUFFER, points, GL_STATIC_DRAW);
+    volumeMesh = gl::VboMesh::create((uint32_t) points.size(), GL_TRIANGLES, {{ layout, vbo }},
+                                     (uint32_t) triangles.size() * 3, GL_UNSIGNED_INT);
+    volumeMesh->bufferIndices(triangles.size() * 3 * sizeof(uint32_t), triangles.data());
+}
 
-    // Verify that the voxel value at (1000, 200000000, -30000000) is
-    // the background value, 0.
-    std::cout << "Grid" << xyz << " = " << accessor.getValue(xyz) << std::endl;
+void BRU12App::gridToMesh(const FloatGridType& grid, vector<openvdb::Vec3s>& points, vector<openvdb::Vec3I>& triangles) {
+    openvdb::tools::VolumeToMesh mesher(0.0, 0.0, true);
+    mesher(grid);
 
-    // Set the voxel value at (1000, 200000000, -30000000) to 2.
-    accessor.setValue(xyz, 2.0);
-    // Set the voxels at the two extremes of the available coordinate space.
-    // For 32-bit signed coordinates these are (-2147483648, -2147483648, -2147483648)
-    // and (2147483647, 2147483647, 2147483647).
-    accessor.setValue(openvdb::Coord::min(), 3.0f);
-    accessor.setValue(openvdb::Coord::max(), 4.0f);
-    std::cout << "Testing sequential access:" << std::endl;
-    // Print all active ("on") voxels by means of an iterator.
-    for (openvdb::FloatGrid::ValueOnCIter iter = grid->cbeginValueOn(); iter; ++iter) {
-        std::cout << "Grid" << iter.getCoord() << " = " << *iter << std::endl;
+    // Preallocate the point list
+    points.clear();
+    points.resize(mesher.pointListSize());
+
+    { // Copy points
+        openvdb::tools::volume_to_mesh_internal::PointListCopy ptnCpy(mesher.pointList(), points);
+        tbb::parallel_for(tbb::blocked_range<size_t>(0, points.size()), ptnCpy);
+        mesher.pointList().reset(nullptr);
+    }
+
+    openvdb::tools::PolygonPoolList& polygonPoolList = mesher.polygonPoolList();
+
+    { // Preallocate primitive lists
+        size_t numQuads = 0, numTriangles = 0;
+        for (size_t n = 0, N = mesher.polygonPoolListSize(); n < N; ++n) {
+            openvdb::tools::PolygonPool& polygons = polygonPoolList[n];
+            numTriangles += polygons.numTriangles();
+            numQuads += polygons.numQuads();
+        }
+
+        triangles.clear();
+        triangles.resize(numTriangles + numQuads * 2);
+    }
+
+    // Copy primitives
+    size_t tIdx = 0;
+    for (size_t n = 0, N = mesher.polygonPoolListSize(); n < N; ++n) {
+        openvdb::tools::PolygonPool& polygons = polygonPoolList[n];
+
+        for (size_t i = 0, I = polygons.numQuads(); i < I; ++i) {
+            openvdb::Vec4I& quad = polygons.quad(i);
+            triangles[tIdx++] = openvdb::Vec3I(quad.x(), quad.y(), quad.z());
+            triangles[tIdx++] = openvdb::Vec3I(quad.x(), quad.z(), quad.w());
+        }
+
+        for (size_t i = 0, I = polygons.numTriangles(); i < I; ++i) {
+            triangles[tIdx++] = polygons.triangle(i);
+        }
     }
 }
 
@@ -196,7 +228,10 @@ void BRU12App::draw() {
         gl::ScopedGlslProg glsl(glslProg);
 //        glPolygonMode(GL_FRONT_AND_BACK, GL_TRIANGLES);
         gl::pointSize(4.0f);
-        gl::draw(mesh);
+//        gl::draw(mesh);
+
+        gl::color(0.0f, 0.0f, 0.0f);
+        gl::draw(volumeMesh);
     }
 
 //    meshBatch->draw();
